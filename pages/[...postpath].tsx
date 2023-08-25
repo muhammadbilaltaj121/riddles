c50a431c5510cb8f36956a18452768c960440f4e
Muhammadbilal Taj <60062343+muhammadbilaltaj121@users.noreply.github.com>
2023-08-26 00:19:46 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint ="https://www.best-riddles.com/graphql"
+	const endpoint =
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					'https://www.best-riddles.com/' + encodeURI(path as string)
+					 + encodeURI(path as string)
 				}`,
 			},
 		};
